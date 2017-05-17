fcd512b6d6eb30bd193fe92e3b3e3e8ad13fc01c
Massimiliano Giorgi <m.giorgi@evidence.eu.com>
2017-05-17 12:28:16 +0200

    extrae debianization

--- scripts/cross-configure-axiom.sh
+++ scripts/cross-configure-axiom.sh
@@ -13,26 +13,70 @@ done
     exit 255;
 }
 
-OUTPUT_DIR=$AXIOMHOME/output
-TARGET_DIR=$(realpath ${OUTPUT_DIR}/target)
-SYSROOT_DIR=$(realpath ${OUTPUT_DIR}/staging)
-HOST_DIR=$(realpath ${OUTPUT_DIR}/host)
+if [ "$P" = "1" ]; then
 
-BUILD_ID='x86_64-unknown-linux-gnu'
-TARGET_ID='aarch64-buildroot-linux-gnu'
+    TARGET_DIR=$(realpath ${ROOTFS})
+#    SYSROOT_DIR=$(realpath ${LINARO}/sysroot)
+    SYSROOT_DIR=$(realpath ${ROOTFS})
+    HOST_DIR=$(realpath ${LINARO}/host)
+        
+    BUILD_ID='x86_64-unknown-linux-gnu'
+    TARGET_ID='aarch64-linux-gnu'
+    
+#    CC="$LINARO/host/usr/bin/aarch64-linux-gnu-gcc" ; export CC
+#    CXX="$LINARO/host/usr/bin/aarch64-linux-gnu-g++" ; export CXX
+#    FC="$LINARO/host/usr/bin/aarch64-linux-gnu-gfortran" ; export FC
+#    export PATH=$PATH:$LINARO/host/usr/bin
+    
+    [ -z "$PREFIX" ] && PREFIX=$HOST_DIR
 
-CC="$HOST_DIR/usr/bin/aarch64-buildroot-linux-gnu-gcc" ; export CC
-CXX="$HOST_DIR/usr/bin/aarch64-buildroot-linux-gnu-g++" ; export CXX
-FC="$HOST_DIR/usr/bin/aarch64-buildroot-linux-gnu-gfortran" ; export FC
+    export PATH=$PATH:$HOST_DIR/usr/bin
+    # questo risolve i problemi alla fine (ma li crea all'inizio)
+    #export LDFLAGS="-Wl,-rpath-link -Wl,${TARGET_DIR}/lib/aarch64-linux-gnu -Wl,-rpath-link -Wl,${TARGET_DIR}/usr/lib/aarch64-linux-gnu         -Wl,-rpath-link -Wl,${TARGET_DIR}/lib -Wl,-rpath-link -Wl,${TARGET_DIR}/usr/lib"
+    
+#    export CFLAGS="-I${ROOTFS}/usr/include/aarch64-linux-gnu"
+#    export LDFLAGS="-L${ROOTFS}/usr/lib/aarch64-linux-gnu -L${ROOTFS}/lib/aarch64-linux-gnu"
+    
+#    export CPPFLAGS="-isystem ${ROOTFS}/usr/include -isystem ${ROOTFS}/usr/include/aarch64-linux-gnu"
+#     export CFLAGS="-L${ROOTFS}/usr/lib -L${ROOTFS}/lib -L${ROOTFS}/usr/lib/aarch64-linux-gnu -L${ROOTFS}/lib/aarch64-linux-gnu"
+    
+    export XML2_CONFIG="$(realpath ${ROOTFS}/usr/bin/xml2-config)"
+#    export CFLAGS="--sysroot=${ROOTFS}"
+#    export CPPFLAGS="--sysroot=${ROOTFS}"
+#    export CXXFLAGS="--sysroot=${ROOTFS}"
+        
+else
+    
+    OUTPUT_DIR=$AXIOMHOME/output
+    TARGET_DIR=$(realpath ${OUTPUT_DIR}/target)
+    SYSROOT_DIR=$(realpath ${OUTPUT_DIR}/staging)
+    HOST_DIR=$(realpath ${OUTPUT_DIR}/host)
 
-PATH=$PATH:$SYSROOT_DIR/usr/bin ; export PATH
+    BUILD_ID='x86_64-unknown-linux-gnu'
+    TARGET_ID='aarch64-buildroot-linux-gnu'
+    
+    CC="$HOST_DIR/usr/bin/aarch64-buildroot-linux-gnu-gcc" ; export CC
+    CXX="$HOST_DIR/usr/bin/aarch64-buildroot-linux-gnu-g++" ; export CXX
+    FC="$HOST_DIR/usr/bin/aarch64-buildroot-linux-gnu-gfortran" ; export FC
 
-[ -z "$PREFIX" ] && PREFIX=$SYSROOT_DIR
+    PATH=$PATH:$SYSROOT_DIR/usr/bin ; export PATH
+    
+    [ -z "$PREFIX" ] && PREFIX=$SYSROOT_DIR
 
+fi
+
+echo $BASEDIR/../configure --prefix=$PREFIX \
+	     --build=$BUILD_ID --host=$TARGET_ID --target=$TARGET_ID \
+	     --enable-aarch64 --with-sysroot=$SYSROOT_DIR \
+	     --with-gnu-ld --without-binutils --disable-xmltest \
+             --enable-nanos --without-mpi --without-unwind \
+	     --without-dyninst --without-papi \
+	     --enable-instrument-dynamic-memory --enable-instrument-io \
+	     "$@"
 $BASEDIR/../configure --prefix=$PREFIX \
 	     --build=$BUILD_ID --host=$TARGET_ID --target=$TARGET_ID \
 	     --enable-aarch64 --with-sysroot=$SYSROOT_DIR \
-	     --without-binutils --disable-xmltest \
+	     --with-gnu-ld --without-binutils --disable-xmltest \
              --enable-nanos --without-mpi --without-unwind \
 	     --without-dyninst --without-papi \
 	     --enable-instrument-dynamic-memory --enable-instrument-io \
